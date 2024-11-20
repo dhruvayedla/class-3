87dc0e88a1cd3a6e7ed60f5bd417e7c115465e4b
dhruvayedla <ysreddy@outlook.com>
2024-11-20 19:19:56 +0000

    Add files via upload

---
+++ Activity 3.py
@@ -0,0 +1,74 @@
+#datatypes
+
+q=45
+print(type(q))
+
+D="Hello my name is dhruva"
+print(type(D))
+
+
+e=134.5
+print(type(e))
+
+w=True
+print(type(w))
+
+
+s="Hello i am 12 years old"
+print(s)
+
+print(type(s))
+
+
+#############################################################################
+#typecasting:-converting one type of data into another
+
+
+age=12
+print(type(age))
+
+
+
+age=str(age)
+print("converted data type: ", type(age))
+
+ufo=1920238
+print(type(ufo))
+
+ufo=float(ufo)
+print("converted ufo data type: ",type(ufo))
+
+
+
+
+print("###################################################################")
+
+##### user input##############
+
+
+country=input("Enter the name of your country: ")
+print(country)
+
+age=input("Enter your age: ")
+print(age)
+
+
+club=input("what is your favourite football club: ")
+print(club)
+
+print("###################################################################")
+
+
+##string operation
+
+
+okne="England"
+
+u=okne[::-1]
+okne=u
+print(okne)
+
+ombt="Manchester united"
+p=ombt[::-1]
+ombt=p
+print(ombt)
